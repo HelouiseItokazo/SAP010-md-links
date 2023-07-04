29c9f32513898cb02323e611ba2d964b29eaec1d
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-07-04 18:07:12 -0300

    inicio dos ajustes options CLI

--- cli.js
+++ cli.js
@@ -1,12 +1,32 @@
 #! /usr/bin/env node
 
-import { mdLinks } from "./index.js";
+import { mdLinks } from './index.js';
 import { argv } from 'node:process';
 
-const [, , path] = argv;
+const [, , path, optionsArgV] = argv;
 
-mdLinks(path)
+const checkOptions = (optionsArgV) => {
+  switch (options) {
+    case '--validate':
+      return {validate: true};
+    case '--stats':
+      return {stats: true}
+    case undefined:
+      break;
+    default:
+    console.log(`Parametros inválidos:
+    Tente:
+      --validate,
+      --stats,
+      --validate --stats`);
+  }
+}
+
+const options = checkOptions(optionsArgV);
+
+mdLinks(path, options)
   .then((files) => {
+    console.log(options)
     for (const data in files) {
       const printFile = files[data].path;
       const printText = files[data].label;
